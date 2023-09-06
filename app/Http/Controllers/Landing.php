cd5c841390d81d148e5fb6575f62431a06589e80
Smirnov AV <smirnov_av@t.top-academy.ru>
2023-09-06 21:26:37 +0300

    landing add page landing

---
+++ app/Http/Controllers/Landing.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Landing extends Controller
+{
+    public function index(Request $request)
+    {
+      $name = $request->query('name', 'Andrew');
+
+      return view('landing', [
+        'name' => $name
+      ]);
+    }
+
+    public function json(Request $request)
+    {
+      $age = $request->query('age', 18);
+
+      return response()->json(['age' => $age]);
+    }
+}
